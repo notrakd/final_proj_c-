26e1a1ec7db66435702d97cac7cf3b9e9fb8c282
notrakd <dmitrytroyt@gmail.com>
2026-04-27 17:39:07 +0300

    Add files via upload

---
+++ Final_project_c++/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "header.h"
+#include <clocale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, ".UTF8");
+    cout << "Обери пункт: ";
+
+    int choice;
+    cout << "1. Выбрать танк и сделать замер" << endl;
+    cout << "2. Показать список всех танков" << endl;
+    cout << "3. Показать таблицу замеров танка" << endl;
+    cout << "4. Рассчитать массу жидкости" << endl;
+    cout << "5. Справка" << endl;
+    cout << "0. Выход" << endl;
+
+    cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Выполняю замер..." << endl;
+                break;
+            case 2:
+                cout << "Список всех танков:" << endl;
+                break;
+            case 3:
+                cout << "Таблица замеров:" << endl;
+                break;
+            case 4:
+                cout << "Расчет массы..." << endl;
+                break;
+            case 5:
+                cout << "Справка по программе..." << endl;
+                break;
+            case 0:
+                cout << "Выход из программы. До свидания!" << endl;
+                break;
+            default:
+                cout << "Ошибка! Выберите пункт от 0 до 5." << endl;
+                break;
+        }
+
+    }
+    
+
+    
