6470e394449d25d742fa0a7806b7605bde24043e
notrakd <dmitrytroyt@gmail.com>
2026-04-28 19:04:05 +0300

    Update main.cpp

--- Final_project_c++/main.cpp
+++ Final_project_c++/main.cpp
@@ -9,6 +9,54 @@ int main()
     setlocale(LC_ALL, ".UTF8");
     cout << "Обери пункт: ";
 
+    int choice = -1;
+    Show_menu();
+
+    while (choice != 0)
+    {
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "Выполняю замер..." << endl;
+                break;
+            case 2:
+                cout << "Список всех танков:" << endl;
+                break;
+            case 3:
+                cout << "Таблица замеров:" << endl;
+                Print_table();
+                Show_menu();
+                break;
+            case 4:
+                cout << "Расчет массы..." << endl;
+                break;
+            case 5:
+                cout << "Справка по программе..." << endl;
+                break;
+            case 0:
+                cout << "Выход из программы" << endl;
+                break;
+            default:
+                cout << "Ошибка! Выберите пункт от 0 до 5." << endl;
+                break;
+        }
+    }
+
+    }
+    
+
+    
+#include <iostream>
+#include "header.h"
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, ".UTF8");
+    cout << "Обери пункт: ";
+
     int choice;
     cout << "1. Выбрать танк и сделать замер" << endl;
     cout << "2. Показать список всех танков" << endl;
